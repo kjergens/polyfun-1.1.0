1d7f5bb155ff516ebb8f853128d3648eb2274662
Katie Jergens <katie.jergens@gmail.com>
2019-06-05 19:22:40 -0400

    Renamed project to polyfun v 2

--- src/test/PolynomialTest.java
+++ src/test/PolynomialTest.java
@@ -11,7 +11,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Random;
 
-// TODO: add test cases with negative terms.
+// TODO: add test cases with negative coefficients.
 public class PolynomialTest {
 
     // Create output streams to capture .print() output.

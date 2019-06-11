349dcce481c0a2dd55c771350ba08a73e673ba59
Katie Jergens <katie.jergens@gmail.com>
2019-06-11 16:09:21 -0400

    Parameterized test for Of (composition) and To (raise to power)

---
+++ src/test/ParameterizedOf.java
@@ -0,0 +1,87 @@
+import org.dalton.polyfun.Polynomial;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import testlib.PolyPairFactory;
+import testlib.PolyPair;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Randomly generate 1000 polynomials in the v6 library and in the v11 library and make sure they match.
+ */
+
+@RunWith(value = Parameterized.class)
+public class ParameterizedOf {
+    private String poly_orig;
+    private String poly_refactored;
+
+    private static final int NUM_TESTS = 1000;
+
+    // Inject via constructor
+    public ParameterizedOf(String poly_orig, String poly_refactored) {
+        this.poly_orig = poly_orig;
+        this.poly_refactored = poly_refactored;
+    }
+
+    @Parameters(name = "{index} {0}")
+    public static Collection<Object[]> data() {
+        // Create list of random polynomials
+        String[][] polyParams = new String[NUM_TESTS][2];
+
+        for (int i = 0; i < polyParams.length; i++) {
+            PolyPair polyPair;
+            PolyPair polyPair2;
+
+            if (i % 5 == 0) {
+                /* Create 20% of the polynomials with the Polynomial(Coef[] coefs) constructor */
+                polyPair = PolyPairFactory.createRandomPolyPairWithCoefArray();
+                polyPair2 = PolyPairFactory.createRandomPolyPairWithCoefArray();
+            } else if (i % 5 == 1) {
+                /* Create 20% with Polynomial(double constant) constructor */
+                polyPair = PolyPairFactory.createRandomPolyPairWithConstant();
+                polyPair2 = PolyPairFactory.createRandomPolyPairWithConstant();
+            } else if (i % 5 == 2) {
+                /* Create 20% with Polynomial(double[] numericalCoefficients) constructor */
+                polyPair = PolyPairFactory.createRandomPolyPairWithDoubleArray();
+                polyPair2 = PolyPairFactory.createRandomPolyPairWithDoubleArray();
+            } else {
+                /* Create 40% with Polynomial(Term term, int degree) constructor */
+                polyPair = PolyPairFactory.createRandomPolyPairWithTermDegree();
+                polyPair2 = PolyPairFactory.createRandomPolyPairWithTermDegree();
+            }
+
+            /* Mutiply the polys */
+            polyfun.Polynomial composition_orig = polyPair.polynomial_orig.of(polyPair2.polynomial_orig);
+            Polynomial composition_refactored = polyPair.polynomial_refactored.of(polyPair2.polynomial_refactored);
+
+            // Get the strings
+
+            // Point System.out to another output stream so I can capture the print() output.
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outContent));
+
+            composition_orig.print();
+            polyParams[i][0] = outContent.toString();
+
+            // Point System.out back to console.
+            System.setOut(originalOut);
+
+            polyParams[i][1] = composition_refactored.toString();
+        }
+
+        return Arrays.asList(polyParams);
+    }
+
+    @Test
+    public void test_RandomPolynomials_Compare_orig_refactored() {
+        Assert.assertEquals(poly_orig, poly_refactored);
+    }
+
+}
---
+++ src/test/ParameterizedTo.java
@@ -0,0 +1,80 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import testlib.PolyPairFactory;
+import testlib.PolyPair;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Randomly generate 1000 polynomials in the original library and in the refactored library and make sure they match.
+ */
+
+@RunWith(value = Parameterized.class)
+public class ParameterizedTo {
+    private String poly_orig;
+    private String poly_refactored;
+    private static final int NUM_TESTS = 1000;
+
+    // Inject via constructor
+    public ParameterizedTo(String poly_orig, String poly_refactored) {
+        this.poly_orig = poly_orig;
+        this.poly_refactored = poly_refactored;
+    }
+
+    @Parameters(name = "{index} {0}")
+    public static Collection<Object[]> data() {
+        // Create list of random polynomials
+        String[][] polyParams = new String[NUM_TESTS][2];
+
+        for (int i = 0; i < polyParams.length; i++) {
+            PolyPair polyPair;
+
+            if (i % 5 == 0) {
+                /* Create 20% of the polynomials with the Polynomial(Coef[] coefs) constructor */
+                polyPair = PolyPairFactory.createRandomPolyPairWithCoefArray();
+            } else if (i % 5 == 1) {
+                /* Create 20% with Polynomial(double constant) constructor */
+                polyPair = PolyPairFactory.createRandomPolyPairWithConstant();
+            } else if (i % 5 == 2) {
+                /* Create 20% with Polynomial(double[] numericalCoefficients) constructor */
+                polyPair = PolyPairFactory.createRandomPolyPairWithDoubleArray();
+            } else {
+                /* Create 40% with Polynomial(Term term, int degree) constructor */
+                polyPair = PolyPairFactory.createRandomPolyPairWithTermDegree();
+            }
+
+            // Create exponent (even spread of positive and negative numbers)
+            int exponent = i - (NUM_TESTS /2);
+            System.err.println(exponent);
+
+            // Get the string
+
+            // Point System.out to another output stream so I can capture the print() output.
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outContent));
+
+            polyPair.polynomial_orig.evaluate(exponent).print();
+            polyParams[i][0] = outContent.toString();
+
+            polyParams[i][1] = polyPair.polynomial_refactored.evaluate(exponent).toString();
+
+            // Point System.out back to console.
+            System.setOut(originalOut);
+        }
+
+        return Arrays.asList(polyParams);
+    }
+
+    @Test
+    public void addTangents_Compare_orig_refactored() {
+        Assert.assertEquals(poly_orig, poly_refactored);
+    }
+
+}
